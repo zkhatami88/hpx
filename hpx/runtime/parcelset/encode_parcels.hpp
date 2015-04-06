c41a5d10175015a46d52b463ca289022bd4c2c8c
John Biddiscombe <biddisco@cscs.ch>
2015-04-06 20:32:08 +0200

    When serializing, pass archive flags to get_type_size
    This allows specializations for classes like serialization_buffer
    to return a different size depending on whether zero_copy is
    enabled or not.

--- hpx/runtime/parcelset/encode_parcels.hpp
+++ hpx/runtime/parcelset/encode_parcels.hpp
@@ -105,7 +105,7 @@ LOG_DEBUG_MSG("Default chunk size is " << hexnumber(chunk_default))
                     {
                         if (arg_size >= max_outbound_size)
                             break;
-                        arg_size += traits::get_type_size(ps[parcels_sent]);
+                        arg_size += traits::get_type_size(ps[parcels_sent],archive_flags_);
                     }
 
                     buffer.data_.reserve((std::max)(chunk_default, arg_size));
